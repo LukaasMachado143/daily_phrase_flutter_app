8bcb85369bae037361fd79bcd5c2472efabca2d9
Lucas Machado <lucas.machado@neogrid.com>
2025-05-02 12:29:16 -0300

    feat: creation of project

---
+++ android/app/src/main/kotlin/com/lks/consultoriadigital/daily_phrases/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lks.consultoriadigital.daily_phrases
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/lks/consutoriadigital/daily_phrases/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lks.consutoriadigital.daily_phrases
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
